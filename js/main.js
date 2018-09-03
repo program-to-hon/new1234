ae55335f166379e620163329fba5390c719e8665
program-to-hon <42829187+program-to-hon@users.noreply.github.com>
2018-09-03 10:58:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,28 @@
 
     var btn = document.getElementById('btn');
 
+    var EVENTNAME_TOUCHSTART, EVENTNAME_TOUCHEND
+
+    if ('ontouchend' in document) {
+    EVENTNAME_TOUCHSTART = 'touchstart';
+    EVENTNAME_TOUCHEND = 'touchend';
+       } else {
+    EVENTNAME_TOUCHSTART = 'mousedown';
+    EVENTNAME_TOUCHEND = 'mouseup';
+    }
+
+    
+
     btn.addEventListener('click', function(){
       this.textContent = '大吉' ;
     });
 
-    btn.addEventListener('mousedown', function(){
+           
+btn.addEventListener(EVENTNAME_TOUCHSTART, function(){
       this.className = 'pushed' ;
     });
 
-    btn.addEventListener('mouseup', function(){
+    btn.addEventListener(EVENTNAME_TOUCHEND, function(){
       this.className = '' ;
     });
 
